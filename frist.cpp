79392a0d6084f168b10a171d57631083f9af8120
New170649 <Pootawon123@gmail.com>
2025-07-01 11:57:14 +0700

    Create frist.cpp

---
+++ frist.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+  string s = "                         ";
+  cout  <<  endl  <<  endl  <<  "\n\n\n\n\n";
+  
+  cout  << s <<  "  **     **  "  << endl;
+  cout  << s <<  "*************"  << endl;
+  cout  << s <<  " ***********"  << endl;
+  cout  << s <<  "    *******"  << endl;
+  cout  << s <<  "      ***"  << endl;
+  cout  << s <<  "       *";
+  }
